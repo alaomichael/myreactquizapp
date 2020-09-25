ed1d3f466ff766888bf7964d5d4dbae484ac32bf
alaomichael <contactleomax@gmail.com>
2020-09-25 23:31:43 +0100

    Updates

---
+++ precache-manifest.3c46ad25c8a0b8b2d57fb6b58fd70c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1818c007ceb0f51d0c090cff2b82ddd1",
+    "url": "/myreactquizapp/index.html"
+  },
+  {
+    "revision": "622b0d1783e4843a1591",
+    "url": "/myreactquizapp/static/css/main.be9c1647.chunk.css"
+  },
+  {
+    "revision": "22240ec8bd6a8a29aba3",
+    "url": "/myreactquizapp/static/js/2.633cca64.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/myreactquizapp/static/js/2.633cca64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622b0d1783e4843a1591",
+    "url": "/myreactquizapp/static/js/main.010bebb3.chunk.js"
+  },
+  {
+    "revision": "44e073995f59c2637606",
+    "url": "/myreactquizapp/static/js/runtime-main.597f2836.js"
+  }
+]);
